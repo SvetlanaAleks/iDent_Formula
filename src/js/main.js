697d606671c27325080dc3f3e082e95664c9d23e
DenysBondarchuk <bondarchuk520@gmail.com>
2019-01-23 13:50:15 +0200

    first

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,6 @@
 //=require ../../node_modules/object-fit-images/dist/ofi.min.js
-
+//=require ../../node_modules/slick-carousel/slick/slick.min.js
 //=require partials/app.js
+//=require partials/advanteges.js
+
+
---
+++ src/js/partials/advanteges.js
@@ -0,0 +1,7 @@
+$('.advanteges__slider').slick({
+	slidesToShow: 1,
+	slidesToScroll: 1
+	// prevArrow: '<button class="advanteges__prev"></button>',
+    // nextArrow: '<button class="advanteges__next"></button>'
+});
+
