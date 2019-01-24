c4679f70bf2663f9bd0a1cb8093127bd6f792434
Svetlana Alekseenko <s.alekseenko@everad.com>
2019-01-24 10:16:35 +0200

    add nav menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,7 @@
 //=require ../../node_modules/object-fit-images/dist/ofi.min.js
 
 //=require partials/app.js
+
+$(document).ready(function() {
+  App.init();
+});
--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -1,1 +1,48 @@
-//Some JS
+const App = (function() {
+  "use strict";
+  const menuBtn = $(".js_menu-show");
+
+  return {
+    shouMainMenu: function() {
+      const menu = $(".menu-wrap");
+      menuBtn.click(function(e) {
+        e.preventDefault();
+        const _this = $(this);
+        _this.toggleClass("menu--active");
+
+        const href = _this.data("target");
+        menu.toggleClass("menu-wrap--active");
+
+        const navMenu = _this.parent(".nav-wrap");
+        const nav = navMenu.parents(".js_nav");
+        nav.toggleClass("nav--active");
+      });
+    },
+    eventScrollMenu: function() {
+      const element = $(".js_none");
+
+      const DOC = $(document);
+      const scrollHeight = 150;
+
+      DOC.scroll(function() {
+        const scrollTop = DOC.scrollTop();
+        const percent = (1 - scrollTop / scrollHeight).toFixed(2);
+        if (percent > 0.95) {
+          element.css("display", "");
+          element.css({ opacity: percent, display: "" });
+          nav.css("background", "");
+          nav.css("box-shadow", "");
+          nav.css({ opacity: percent, background: "" });
+        } else {
+          element.css("display", "block");
+          nav.css("background", "white");
+          nav.css("box-shadow", "10px 17.3px 35px 0 rgba(66, 97, 106, 0.2)");
+        }
+      });
+    },
+    init: function() {
+      App.shouMainMenu();
+      App.eventScrollMenu();
+    }
+  };
+})();
